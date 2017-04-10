78dd50291f3dde378059dc82303d8b5f292c5734
edward ingram <ed@edwardingram.com>
2017-04-10 03:39:57 -0400

    Add logic around web fonts

--- functions.php
+++ functions.php
@@ -72,6 +72,43 @@ endif;
 add_action( 'after_setup_theme', 'unquieted_setup' );
 
 /**
+ * Register custom fonts.
+ */
+function unquieted_fonts_url() {
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Codystar and Open Sans, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$codystar = _x( 'on', 'Codystar font: on or off', 'unquieted' );
+	$open_sans = _x( 'on', 'Open Sans font: on or off', 'unquieted' );
+
+        $font_families = array();
+        
+	if ( 'off' !== $codystar ) {	
+		$font_families[] = 'Codystar';
+        }
+
+	if ( 'off' !== $open_sans ) {	
+		$font_families[] = 'Open+Sans:400,400i,800';
+        }
+ 
+        if (in_array( 'on', array($codystar, $open_sans)) ) {
+		
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
@@ -107,7 +144,7 @@ add_action( 'widgets_init', 'unquieted_widgets_init' );
 function unquieted_scripts() {
     
         //Enqueue Google Fonts
-        wp_enqueue_style('unquieted-fonts', 'https://fonts.googleapis.com/css?family=Codystar|Open+Sans:400,400i,800');
+        wp_enqueue_style('unquieted-fonts', unquieted_fonts_url());
 
 	wp_enqueue_script( 'unquieted-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
@@ -118,6 +155,29 @@ function unquieted_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'unquieted_scripts' );
+
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function unquieted_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'unquieted-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'unquieted_resource_hints', 10, 2 );
+
 
 /**
  * Implement the Custom Header feature.
