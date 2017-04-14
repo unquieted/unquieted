8f3a576d4688416a3fa7e70f3783346e39392267
edward ingram <ed@edwardingram.com>
2017-04-14 04:30:54 -0400

    Add dropdowns to menu

--- functions.php
+++ functions.php
@@ -154,9 +154,14 @@ function unquieted_scripts() {
         //Enqueue Google Fonts
         wp_enqueue_style('unquieted-fonts', unquieted_fonts_url());
 
-	wp_enqueue_script( 'unquieted-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'unquieted-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
 
-	wp_enqueue_script( 'unquieted-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_localize_script('unquieted-navigation', 'unquietedScreenReaderText', array(
+            'expand' => __('Expand child menu', 'unquieted'),
+            'collapse' => __('Collapse child menu', 'unquieted'),
+        ));
+        
+        wp_enqueue_script( 'unquieted-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
