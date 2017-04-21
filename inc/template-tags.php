067d18223fe59f686412cfdc944364fe4ef39006
edward ingram <ed@edwardingram.com>
2017-04-21 06:36:21 -0400

    Style and add accessible text to post navigation

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -128,3 +128,17 @@ function unquieted_category_transient_flusher() {
 }
 add_action( 'edit_category', 'unquieted_category_transient_flusher' );
 add_action( 'save_post',     'unquieted_category_transient_flusher' );
+
+/**
+ * Post navigation (previous / next post) for single posts.
+ */
+function unquieted_post_navigation() {
+	the_post_navigation( array(
+		'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'unquieted' ) . '</span> ' .
+			'<span class="screen-reader-text">' . __( 'Next post:', 'unquieted' ) . '</span> ' .
+			'<span class="post-title">%title</span>',
+		'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'unquieted' ) . '</span> ' .
+			'<span class="screen-reader-text">' . __( 'Previous post:', 'unquieted' ) . '</span> ' .
+			'<span class="post-title">%title</span>',
+	) );
+}
