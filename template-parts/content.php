81ce5b1c156f2b7fc5a903fe9d728fcda44b5453
edward ingram <ed@edwardingram.com>
2017-04-27 09:57:14 -0400

    Customize the customizer

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template part for displaying posts
+ * Template part for displaying posts.
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -10,55 +10,60 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    
-                <?php
-                if( has_post_thumbnail() ) { ?>
-        <figure class="index-image">
-            <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
-                <?php
-                    the_post_thumbnail('unquieted-index-img');
-                ?>
-            </a>
-        </figure>
-                <?php } ?>
-    <div class="post__content">
-	<header class="entry-header">
-            
-                <?php unquieted_the_category_list(); ?>
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+	
+	<?php
+	if ( has_post_thumbnail() ) { ?>
+	<figure class="featured-image index-image">
+		<a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+			<?php
+			the_post_thumbnail('unquieted-index-img');
+			?>
+		</a>
+	</figure><!-- .featured-image full-bleed -->
+	<?php } ?>
+	
+	<div class="post__content">
+		<header class="entry-header">
+			<?php unquieted_the_category_list(); ?>
+			<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			endif;
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php unquieted_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php unquieted_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
+		</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-content -->
-        
-        <div class="continue-reading">
-            <?php 
-                $read_more_link = sprintf(
-                    /* translators: %s: Name of current post. */
-                    wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'unquieted' ), array( 'span' => array( 'class' => array() ) ) ),
-                    the_title( '<span class="screen-reader-text">"', '"</span>', false )); 
-            ?>
-            <a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
-                <?php echo $read_more_link; ?>
-            </a>
-            
-        </div><!-- .continue-reading -->
-
-	<!-- <footer class="entry-footer">
-		<?php unquieted_entry_footer(); ?>
-	</footer> --><!-- .entry-footer -->
-    </div><!-- .post__content -->
+		<div class="entry-content">
+			<?php
+			$length_setting = get_theme_mod('length_setting');
+			if ( 'excerpt' === $length_setting ) {
+				the_excerpt();
+			} else {
+				the_content();
+			}
+			?>
+		</div><!-- .entry-content -->
+		
+		<div class="continue-reading">
+			<?php
+			$read_more_link = sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s', 'unquieted' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			);
+			?>
+					
+			<a href="<?php echo esc_url( get_permalink() ) ?>" rel="bookmark">
+				<?php echo $read_more_link; ?>
+			</a>
+		</div><!-- .continue-reading -->
+		
+	</div><!-- .post__content -->
 </article><!-- #post-## -->
