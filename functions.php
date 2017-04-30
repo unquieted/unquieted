641d1ac44ce982b8f2cefd2bee92f2e68e588930
edward ingram <ed@edwardingram.com>
2017-04-29 23:33:07 -0400

    Customize the customizer part 2

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * unquieted functions and definitions
+ * unquieted functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
@@ -41,13 +41,13 @@ function unquieted_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
-        
-        add_image_size( 'unquieted-index-img', 800, 450, true);
+	add_image_size( 'unquieted-full-bleed', 2000, 1200, true );
+	add_image_size( 'unquieted-index-img', 1000, 550, true );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'menu-1' => esc_html__( 'Header', 'unquieted' ),
-                'menu-social' => esc_html__( 'Social Media Menu', 'unquieted'),
+		'primary' => esc_html__( 'Header', 'unquieted' ),
+		'social' => esc_html__( 'Social Media Menu', 'unquieted' ),
 	) );
 
 	/*
@@ -67,20 +67,20 @@ function unquieted_setup() {
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
-        
-        // Add theme support for custom logo
-        
-        add_theme_support( 'custom-logo', array(
-            'width' => 90,
-            'height' => 90,
-            'flex-width' => true,
-        ));
 
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	// Add theme support for Custom Logo
+	add_theme_support( 'custom-logo', array(
+		'width' => 90,
+		'height' => 90,
+		'flex-width' => true,
+	));
+
+	/* Editor styles */
+	add_editor_style( array( 'inc/editor-styles.css', unquieted_fonts_url() ) );
 }
 endif;
 add_action( 'after_setup_theme', 'unquieted_setup' );
+
 
 /**
  * Register custom fonts.
@@ -90,24 +90,25 @@ function unquieted_fonts_url() {
 
 	/**
 	 * Translators: If there are characters in your language that are not
-	 * supported by Codystar and Open Sans, translate this to 'off'. Do not translate
+	 * supported by Source Sans Pro and PT Serif, translate this to 'off'. Do not translate
 	 * into your own language.
 	 */
-	$codystar = _x( 'on', 'Codystar font: on or off', 'unquieted' );
-	$open_sans = _x( 'on', 'Open Sans font: on or off', 'unquieted' );
+	$source_sans_pro = _x( 'on', 'Source Sans Pro font: on or off', 'unquieted' );
+	$pt_serif = _x( 'on', 'PT Serif font: on or off', 'unquieted' );
 
-        $font_families = array();
-        
-	if ( 'off' !== $codystar ) {	
-		$font_families[] = 'Codystar';
-        }
+	$font_families = array();
 
-	if ( 'off' !== $open_sans ) {	
-		$font_families[] = 'Open+Sans:400,400i,800';
-        }
- 
-        if (in_array( 'on', array($codystar, $open_sans)) ) {
-		
+	if ( 'off' !== $source_sans_pro ) {
+		$font_families[] = 'Source Sans Pro:400,400i,700,900';
+	}
+
+	if ( 'off' !== $pt_serif ) {
+		$font_families[] = 'PT Serif:400,400i,700,700i';
+	}
+
+
+	if ( in_array( 'on', array($source_sans_pro, $pt_serif) ) ) {
+
 		$query_args = array(
 			'family' => urlencode( implode( '|', $font_families ) ),
 			'subset' => urlencode( 'latin,latin-ext' ),
@@ -120,6 +121,27 @@ function unquieted_fonts_url() {
 }
 
 /**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function unquieted_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'unquieted-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'unquieted_resource_hints', 10, 2 );
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
@@ -130,6 +152,7 @@ function unquieted_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'unquieted_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'unquieted_content_width', 0 );
+
 
 /**
  * Add custom image sizes attribute to enhance responsive image functionality
@@ -203,6 +226,8 @@ function unquieted_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
 add_filter( 'wp_get_attachment_image_attributes', 'unquieted_post_thumbnail_sizes_attr', 10, 3 );
 
 
+
+
 /**
  * Register widget area.
  *
@@ -212,12 +237,33 @@ function unquieted_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'unquieted' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'unquieted' ),
+		'description'   => esc_html__( 'Add sidebar widgets here.', 'unquieted' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Page Sidebar', 'unquieted' ),
+		'id'            => 'sidebar-2',
+		'description'   => esc_html__( 'Add page sidebar widgets here.', 'unquieted' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Footer Widgets', 'unquieted' ),
+		'id'            => 'footer-1',
+		'description'   => esc_html__( 'Add footer widgets here.', 'unquieted' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
 }
 add_action( 'widgets_init', 'unquieted_widgets_init' );
 
@@ -225,50 +271,26 @@ add_action( 'widgets_init', 'unquieted_widgets_init' );
  * Enqueue scripts and styles.
  */
 function unquieted_scripts() {
-    
-        //Enqueue Google Fonts
-        wp_enqueue_style('unquieted-fonts', unquieted_fonts_url());
+	// Enqueue Google Fonts: Source Sans Pro and PT Serif
+	wp_enqueue_style( 'unquieted-fonts', unquieted_fonts_url() );
+
+	wp_enqueue_style( 'unquieted-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'unquieted-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
+	wp_localize_script( 'unquieted-navigation', 'unquietedScreenReaderText', array(
+		'expand' => __( 'Expand child menu', 'unquieted'),
+		'collapse' => __( 'Collapse child menu', 'unquieted'),
+	));
 
-	wp_localize_script('unquieted-navigation', 'unquietedScreenReaderText', array(
-            'expand' => __('Expand child menu', 'unquieted'),
-            'collapse' => __('Collapse child menu', 'unquieted'),
-        ));
-        
-   	wp_enqueue_script( 'unquieted-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20161201', true );
+	wp_enqueue_script( 'unquieted-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20161201', true );
 
-        
-        wp_enqueue_script( 'unquieted-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'unquieted-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'unquieted_scripts' );
-
-
-/**
- * Add preconnect for Google Fonts.
- *
- * @since Twenty Seventeen 1.0
- *
- * @param array  $urls           URLs to print for resource hints.
- * @param string $relation_type  The relation type the URLs are printed.
- * @return array $urls           URLs to print for resource hints.
- */
-function unquieted_resource_hints( $urls, $relation_type ) {
-	if ( wp_style_is( 'unquieted-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
-		$urls[] = array(
-			'href' => 'https://fonts.gstatic.com',
-			'crossorigin',
-		);
-	}
-
-	return $urls;
-}
-add_filter( 'wp_resource_hints', 'unquieted_resource_hints', 10, 2 );
-
 
 /**
  * Implement the Custom Header feature.
