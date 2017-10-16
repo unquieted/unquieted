a6b786d26a0910c08eb3f2c553d5e5c2e53cd266
edward ingram <ed@edwardingram.com>
2017-10-16 17:58:24 -0400

    Changed typo in name of menu location

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'unquieted' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
